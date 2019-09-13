84ddb105e2853576fa82588cbdc281a58b9ae03a
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-09-13 15:46:59 -0600

    just trying to get heroku to work

--- Server.js
+++ Server.js
@@ -11,11 +11,8 @@ ranks = [{
 }]
 
 // Serve static files from the React app
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.json());
-
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on  http://localhost:${port}/`));
 
 app.get('api/ranks', (req, res) => {
   app.json(ranks)
@@ -33,7 +30,10 @@ app.post('api/ranks', (req, res) => {
   }
 })
 
-
 app.get('*', (req, res) => {
-  res.sendFile('client/build/index.html');
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on  http://localhost:${port}/`));
+
