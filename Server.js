4d961ffd97f5522fbe2b29c947904ecdbd163cc5
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-09-13 15:39:12 -0600

    fixing file served

--- Server.js
+++ Server.js
@@ -35,5 +35,5 @@ app.post('api/ranks', (req, res) => {
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile('client/build/index.html');
 });
