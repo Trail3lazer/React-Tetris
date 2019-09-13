90d6aaf3f210eeed3bb92d3eb0fada33da842d0f
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-09-13 14:50:04 -0600

    fixed  * route

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const app = express();
+const path = require("path")
 const port = process.env.PORT || 5000;
 
 ranks = [{
@@ -12,7 +13,7 @@ ranks = [{
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
